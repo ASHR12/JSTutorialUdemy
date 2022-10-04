6ecbfaad9825d33d5890f8fa3bf22a0796778f4a
ASHR12 <shahidshrivastava.01@gmail.com>
2022-10-05 00:41:12 +0530

    fixed height issue.

--- JS_Mini_Project/27-ashu-pay/util/nav.js
+++ JS_Mini_Project/27-ashu-pay/util/nav.js
@@ -1,8 +1,6 @@
 const setNavEventListener = function (navElement, subMenu) {
   navElement.addEventListener("mouseover", function (e) {
     if (!e.target.classList.contains("nav-btn")) {
-      console.log("inside if nav.js");
-      console.log(e.target);
       subMenu.classList.remove("show");
     }
   });
